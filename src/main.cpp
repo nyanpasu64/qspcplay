21b4e841973f0d30cc1ea9b4d440b084a0f8272c
nyanpasu64 <nyanpasu64@tuta.io>
2021-10-03 01:57:38 -0700

    Drop support for Qt 5, fix multi-version warning on Qt 6
    On Qt 6, specifying version numbers for QtQuick.* import statements led
    to error "QQmlEngine::setContextForObject(): Object already has a
    QQmlContext", caused by importing multiple versions of the same module.
    This drops support for Qt 5, requiring Qt 6 only, to simplify
    programming.

--- src/main.cpp
+++ src/main.cpp
@@ -60,25 +60,12 @@ static Result<QSqlDatabase, QString> open_settings() {
 }
 
 int main(int argc, char *argv[]) {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
-
     QGuiApplication::setHighDpiScaleFactorRoundingPolicy(
         Qt::HighDpiScaleFactorRoundingPolicy::RoundPreferFloor
     );
     QGuiApplication app(argc, argv);
 
-    // The default QQC2 style on Qt 5 is Basic, which is ugly.
     // The default style on Qt 6 is Fusion, which looks OK.
-    // However, on Qt 6, it's not accessible by the name "fusion", so don't set it.
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    // If QT_QUICK_CONTROLS_STYLE is set, use the style override.
-    // Otherwise, default to Fusion.
-    if (qEnvironmentVariableIsEmpty("QT_QUICK_CONTROLS_STYLE")) {
-        QQuickStyle::setStyle("fusion");
-    }
-#endif
 
     QSqlDatabase settings;
 
